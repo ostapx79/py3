787082629488de2e8820eab3c27edc9ad99cf8de
Constantin Galus <ostapx79@gmail.com>
2020-10-16 01:01:06 +0300

    узнали какое целое число находится в константа RAND_MAX в Си++

---
+++ lesson2/c_c++/homework/homework1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+
+int main()
+{
+    // какое заначение у константа RAND_MAX;
+    using namespace std;
+    int rand_const;
+    rand_const = RAND_MAX;
+
+    cout << rand_const << endl;
+
+    return 0;
+}
