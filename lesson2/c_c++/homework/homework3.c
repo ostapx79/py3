d6c0685e833570572edf23ff3a02001aa0afc675
Constantin Galus <ostapx79@gmail.com>
2020-10-16 11:40:00 +0300

    вычисляем радиус круга в Си

--- lesson2/c_c++/homework/homework3.c
+++ lesson2/c_c++/homework/homework3.c
@@ -3,7 +3,15 @@
 
 int main()
 {
-    printf("%.7lf\n", M_PI);
+    // Напишите программу, которая вводит радиус круга и вычисляет его площадь и
+    // длину окржности.
+    double radius, aria, circumference;
+    printf("Введите радиус круга\n");
+    scanf("%lf", &radius);
+
+    aria = M_PI * radius * radius;
+    circumference = 2 * M_PI * radius;
+    printf("%.3lf\n%.3lf\n", aria, circumference);
 
     return 0;
 }
