dcd487c9c059973d09664be433f9c99bc97234f6
Constantin Galus <ostapx79@gmail.com>
2020-10-16 12:13:35 +0300

    д/з упражнение номер 2 дописываем по Си и Си++

--- lesson2/c_c++/homework/homework2.cpp
+++ lesson2/c_c++/homework/homework2.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <iostream>
 
 int main()
 {
@@ -7,20 +7,24 @@ int main()
     // вводе чисел 4, 5 и 7 мы должны получить ответ, 4+5+7=16, 4*5*7=140,
     // (4+5+7)/3=5.333333.
 
+    using namespace std;
+
     int inp_numb1, inp_numb2, inp_numb3, inp_numb4, res_int;
     inp_numb4 = 3;
     float res_float;
 
-    scanf("%d%d%d", &inp_numb1, &inp_numb2, &inp_numb3);
+    cin >> inp_numb1 >> inp_numb2 >> inp_numb3;
 
     res_int = inp_numb1 + inp_numb2 + inp_numb3;
-    printf("%d+%d+%d=%d\n", inp_numb1, inp_numb2, inp_numb3, res_int);
+    cout << inp_numb1 << '+' << inp_numb2 << '+' << inp_numb3 << '=' << res_int << endl;
 
     res_int = inp_numb1 * inp_numb2 * inp_numb3;
-    printf("%d+%d+%d=%d\n", inp_numb1, inp_numb2, inp_numb3, res_int);
+    cout << inp_numb1 << '*' << inp_numb2 << '*' << inp_numb3 << '=' << res_int << endl;
 
     res_float = (inp_numb1 + inp_numb2 + inp_numb3) / (float)inp_numb4;
-    printf("(%d+%d+%d)/%d=%f\n", inp_numb1, inp_numb2, inp_numb3, inp_numb4, res_float);
+    cout.setf(ios::fixed);
+    cout.precision(6);
+    cout << '(' << inp_numb1 << '+' << inp_numb2 << '+' << inp_numb3 << ")/" << inp_numb4 << '=' << res_float << endl;
 
     return 0;
 }
