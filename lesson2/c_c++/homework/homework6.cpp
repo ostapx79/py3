4c487eadaabb162566853f1e212ff32cf817cebe
ConstantinGalus <ostapx79@gmail.com>
2020-10-17 15:03:19 +0300

    вычиляем целое число в степень 10, в Си и Си++ и переполнение типа даных

---
+++ lesson2/c_c++/homework/homework6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    // Описание к этому файлу находится в файле hovework6.c
+    using namespace std;
+
+    int numb1, numb2;
+
+    cin >> numb1;
+
+    numb2 = pow(numb1, 10);
+
+    cout << numb2 << endl;
+
+    return 0;
+}
---
+++ lesson2/c_c++/homework/homework7.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    // Описание к этому файлу находится в файле homework7.c, если возникнут
+    // вопросы,
+    
+
+    using namespace std;
+    
+    int numb1;
+    long long numb2;
+
+    cin >> numb1;
+
+    numb2 = pow(numb1, 10);
+
+    cout << numb2 << endl;
+
+    return 0;
+}
