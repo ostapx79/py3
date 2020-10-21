563632286244d49ce31716a1e98e6b299b64abac
ConstantinGalus <ostapx79@gmail.com>
2020-10-21 21:25:17 +0300

    д/з по Си, упражнение 16

---
+++ lesson2/c_c++/homework/homework17.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    // Напишите программу, которая вводит два вещественных числа, a и b (a < b),
+    // и выводит на экран 5 случайных вещественные чиисла [a, b].
+    
+    srand(time(NULL));
+    float first_float_numb, second_float_numb;
+
+    printf("Введите перевое вещественное число => ");
+    scanf("%f", &first_float_numb);
+    printf("Введите второе вещественное число => ");
+    scanf("%f", &second_float_numb);
+
+    printf("Превое случайное вещественное число => %.2f\n", first_float_numb + (second_float_numb - first_float_numb) * rand() / RAND_MAX);
+    printf("Второе случайное вещественное число => %.2f\n", first_float_numb + (second_float_numb - first_float_numb) * rand() / RAND_MAX);
+    printf("Третие случайное вещественное число => %.2f\n", first_float_numb + (second_float_numb - first_float_numb) * rand() / RAND_MAX);
+    printf("Четвертое случайное вещественное число => %.2f\n", first_float_numb + (second_float_numb -first_float_numb) * rand() / RAND_MAX);
+    printf("Пятое случайное вещественное число => %.2f\n", first_float_numb + (second_float_numb - first_float_numb) * rand() / RAND_MAX);
+
+    return 0;
+}
--- lesson2/c_c++/homework/homework_test.c
+++ lesson2/c_c++/homework/homework_test.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
+    srand(time(NULL));
+    float first_value, second_value;
 
-    float float_value;
-
-    scanf("%f", &float_value);
-    printf("%.f\n", float_value);
+    scanf("%f", &first_value);
+    scanf("%f", &second_value);
+    printf("%.2f\n", first_value + (second_value - first_value) * rand() / RAND_MAX);
 
     return 0;
 }
