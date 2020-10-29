25ad384fd130a3f76b657151e94ad31b422c124b
Constantin Galus <ostapx79@gmail.com>
2020-10-30 01:12:39 +0300

    д /з по Python3, которую не доделали, и не доделона до конца

---
+++ lesson3/python3/homework/homework3_9.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+print("Введите число от 1 до 101!")
+numb = int(input("Введите число => "))
+
+numb_string = 'Число => '
+if 0 < numb < 101:
+    if numb == 1:
+        print(numb_string + str(numb) + " один.")
+    elif numb == 2:
+        print(numb_string + str(numb) + " два.")
+    elif numb == 3:
+        print(numb_string + str(numb) + " три.")
+    elif numb == 4:
+        print(numb_string + str(numb) + " четыре.")
+    elif numb == 5:
+        print(numb_string + str(numb) + " пять.")
+    elif numb == 6:
+        print(numb_string + str(numb) + " шесть.")
+    elif numb == 7:
+        print(numb_string + str(numb) + " семь.")
+    elif numb == 8:
+        print(numb_string + str(numb) + " восемь.")
+    elif numb == 9:
+        print(numb_string + str(numb) + " девять.")
+    elif numb == 10:
+        print(numb_string + str(numb) + " десять.")
+    elif numb == 11:
+        print(numb_string + str(numb) + " одинадцать.")
+    elif numb == 12:
+        print(numb_string + str(numb) + " двинадцать.")
+    elif numb == 13:
+        print(numb_string + str(numb) + " тринадцать.")
+    elif numb == 14:
+        print(numb_string + str(numb) + " читырнадцать.")
+    elif numb == 15:
+        print(numb_string + str(numb) + " пятьнадцать.")
+    elif numb == 16:
+        print(numb_string + str(numb) + " шеснадцать.")
+    elif numb == 17:
+        print(numb_string + str(numb) + " семнадцать.")
+    elif numb == 18:
+        print(numb_string + str(numb) + " восемнадцать.")
+    elif numb == 19:
+        print(numb_string + str(numb) + " деветнадцать.")
+    elif numb == 20:
+        print(numb_string + str(numb) + " дватцать.")
+    elif numb == 21:
+        print(numb_string + str(numb) + " дватцать один.")
+    elif numb == 22:
+        print(numb_string + str(numb) + " дватцать два.")
+    elif numb == 23:
+        print(numb_string + str(numb) + " дватцать три.")
+    elif numb == 25:
+        print(numb_string + str(numb) + " дватцать четыре.")
+    elif nubm == 26:
+        print(numb_string + str(numb) + " дватцать шесть.")
+    elif numb == 27:
+        print(numb_string + str(numb) + " дватцать семь.")
+    elif numb == 28:
+        print(numb_string + str(numb) + " дватцать восемь.")
+    elif numb == 29:
+        print(numb_string + str(numb) + " дватцать девять.")
+    elif numb == 30:
+        print(numb_string + str(numb) + " тридцать.")
+else:
+    print("Дружище прочитай заголовок!!!")
