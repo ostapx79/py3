59abc383434fb5c241e02ac1d5458a70cd7236e8
ConstantinGalus <ostapx79@gmail.com>
2020-10-29 21:02:19 +0300

    д/з по Си Си++ и Python3, урока 3 упражнение 9, по Python3 надо дописать

---
+++ lesson3/c_c++/homework/homework3_9.c
@@ -0,0 +1,220 @@
+#include <stdio.h>
+
+int main() {
+    
+    // Напишите программу, которая вводит целое число, не превышая 100, и
+    // выводит его прописью, например 21 -> "дватцать один".
+
+    int numb;
+    printf("Введите число от 1 до 100!\n");
+    printf("number => ");
+    scanf("%d", &numb);
+
+    if (numb > 0 && numb < 101) {
+        if (numb == 1){
+            printf("Число %d один.\n", numb);
+        } else if ( numb == 2) {
+            printf("Число %d => два.\n", numb);
+        } else if (numb == 3) {
+            printf("Число %d => три.\n", numb);
+        } else if (numb == 4) {
+            printf("Число %d => читыре.\n", numb);
+        } else if (numb == 5) {
+            printf("Число %d => пять.\n", numb);
+        } else if (numb == 6) {
+            printf("Число %d => шесть.\n", numb);
+        } else if (numb == 7) {
+            printf("Число %d => семь.\n", numb);
+        } else if (numb == 8) {
+            printf("Число %d => восемь.\n", numb);
+        } else if (numb == 9) {
+            printf("Число %d => девять.\n", numb);
+        } else if (numb == 10) {
+            printf("Число %d => десять.\n", numb);
+        } else if (numb == 11) {
+            printf("Число %d => одинацать.\n", numb);
+        } else if (numb == 12) {
+            printf("Число %d => двенадцать.\n", numb);
+        } else if (numb == 13) {
+            printf("Число %d => тринадцать.\n", numb);
+        } else if (numb == 14) {
+            printf("Число %d => четырнадцать.\n", numb);
+        } else if (numb == 15) {
+            printf("Число %d => пятнадцать.\n", numb);
+        } else if (numb == 16) {
+            printf("Число %d => шестьнадцать.\n", numb);
+        } else if (numb == 17) {
+            printf("Число %d => семьнадцать.\n", numb);
+        } else if (numb == 18) {
+            printf("Число %d => восемьнадцать.\n", numb);
+        } else if (numb == 19) {
+            printf("Число %d => девятьнадцать.\n", numb);
+        } else if (numb == 20) {
+            printf("Число %d => двадцать.\n", numb);
+        } else if (numb == 21) {
+            printf("Число %d => двадцать один.\n", numb);
+        } else if (numb == 22) {
+            printf("Число %d => двадцать два.\n", numb);
+        } else if (numb == 23) {
+            printf("Число %d => двадцать три.\n", numb);
+        } else if (numb == 24) {
+            printf("Число %d => двадцать четыре.\n", numb);
+        } else if (numb == 25) {
+            printf("Число %d => двадцать пять.\n", numb);
+        } else if (numb == 26) {
+            printf("Число %d => двадцать шесть.\n", numb);
+        } else if (numb == 27) {
+            printf("Число %d => двадцать семь.\n", numb);
+        } else if (numb == 28) {
+            printf("Число %d => двадцать восемь.\n", numb);
+        } else if (numb == 29) {
+            printf("Число %d => двадцать дявять.\n", numb);
+        } else if (numb == 30) {
+            printf("Число %d => тридцать.\n", numb);
+        } else if (numb == 31) {
+            printf("Число %d => тридцать один.\n", numb);
+        } else if (numb == 32) {
+            printf("Число %d => тридцать два.\n", numb);
+        } else if (numb == 33) {
+            printf("Число %d => тридцать три.\n", numb);
+        } else if (numb == 34) {
+            printf("Число %d => тридцать читыри.\n", numb);
+        } else if (numb == 35) {
+            printf("Число %d => тридцать пять.\n", numb);
+        } else if (numb == 36) {
+            printf("Число %d => тридцать шесть.\n", numb);
+        } else if (numb == 37) {
+            printf("Число %d => тридцать семь.\n", numb);
+        } else if (numb == 38) {
+            printf("Число %d => тридцать восемь.\n", numb);
+        } else if (numb == 39) {
+            printf("Число %d => тридцать девять.\n", numb);
+        } else if (numb == 40) {
+            printf("Число %d => сорок.\n", numb);
+        } else if (numb == 41) {
+            printf("Число %d => сорок один.\n", numb);
+        } else if (numb == 42) {
+            printf("Число %d => сорок два.\n", numb);
+        } else if (numb == 43) {
+            printf("Число %d => сорок три.\n", numb);
+        } else if (numb == 44) {
+            printf("Число %d => сорок четыри.\n", numb);
+        } else if (numb == 45) {
+            printf("Число %d => сорок пять.\n", numb);
+        } else if (numb == 46) {
+            printf("Число %d => сорок шесть.\n", numb);
+        } else if (numb == 47) {
+            printf("Число %d => сорок семь.\n", numb);
+        } else if (numb == 48) {
+            printf("Число %d => сорок восемь.\n", numb);
+        } else if (numb == 49) {
+            printf("Число %d => сорок девять.\n", numb);
+        } else if (numb == 50) {
+            printf("Число %d => педисят.\n", numb);
+        } else if (numb == 51) {
+            printf("Число %d => педисят один.\n", numb);
+        } else if (numb == 52) {
+            printf("Число %d => педисят два.\n", numb);
+        } else if (numb == 53) {
+            printf("Число %d => педисят три.\n", numb);
+        } else if (numb == 54) {
+            printf("Число %d => педисят четыри.\n", numb);
+        } else if (numb == 55) {
+            printf("Число %d => педисят пять.\n", numb);
+        } else if (numb == 56) {
+            printf("Число %d => педисят шесть.\n", numb);
+        } else if (numb == 57) {
+            printf("Число %d => педисят семь.\n", numb);
+        } else if (numb == 58) {
+            printf("Число %d => педисят восемь.\n", numb);
+        } else if (numb == 59) {
+            printf("Число %d => педисят девять.\n", numb);
+        } else if (numb == 60) {
+            printf("Число %d => шестдисят.\n", numb);
+        } else if (numb == 61) {
+            printf("Число %d => шестдисят один.\n", numb);
+        } else if (numb == 62) {
+            printf("Число %d => шестдисят два.\n", numb);
+        } else if (numb == 63) {
+            printf("Число %d => шестдисят три.\n", numb);
+        } else if (numb == 64) {
+            printf("Число %d => шестдисят четыри.\n", numb);
+        } else if (numb == 65) {
+            printf("Число %d => шестдисят пять.\n", numb);
+        } else if (numb == 66) {
+            printf("Число %d => шестдисят шесть.\n", numb);
+        } else if (numb == 67) {
+            printf("Число %d => шестдисят семь.\n", numb);
+        } else if (numb == 68) {
+            printf("Число %d => шестдисят восемь.\n", numb);
+        } else if (numb == 69) {
+            printf("Число %d => шестдисят девять.\n", numb);
+        } else if (numb == 70) {
+            printf("Число %d => семьдесят.\n", numb);
+        } else if (numb == 71) {
+            printf("Число %d => семьдесят один.\n", numb);
+        } else if (numb == 72) {
+            printf("Число %d => семьдесят два.\n", numb);
+        } else if (numb == 73) {
+            printf("Число %d => семьдесят три.\n", numb);
+        } else if (numb == 74) {
+            printf("Число %d => семьдесят четыре.\n", numb);
+        } else if (numb == 75) {
+            printf("Число %d => семьдесят пять.\n", numb);
+        } else if (numb == 76) {
+            printf("Число %d => семьдесят шесть.\n", numb);
+        } else if (numb == 77) {
+            printf("Число %d => семьдесят семь.\n", numb);
+        } else if (numb == 78) {
+            printf("Число %d => семьдесят восимь.\n", numb);
+        } else if (numb == 79) {
+            printf("Число %d => семьдесят девять.\n", numb);
+        } else if (numb == 80) {
+            printf("Число %d => восемьдесят.\n", numb);
+        } else if (numb == 81) {
+            printf("Число %d => восемьдесят один.\n", numb);
+        } else if (numb == 82) {
+            printf("Число %d => восемьдесят два.\n", numb);
+        } else if (numb == 83) {
+            printf("Число %d => восемьдесят три.\n", numb);
+        } else if (numb == 84) {
+            printf("Число %d => восемьдесят четыре.\n", numb);
+        } else if (numb == 85) {
+            printf("Число %d => восемьдесят пять.\n", numb);
+        } else if (numb == 86) {
+            printf("Число %d => восемьдесят шесть.\n", numb);
+        } else if (numb == 87) {
+            printf("Число %d => восемьдесят семь.\n", numb);
+        } else if (numb == 88) {
+            printf("Число %d => восемьдесят восемь.\n", numb);
+        } else if (numb == 89) {
+            printf("Число %d => восемьдесят девять.\n", numb);
+        } else if (numb == 90) {
+            printf("Число %d => девяносто.\n", numb);
+        } else if (numb == 91) {
+            printf("Число %d => девяносто оди.\n", numb);
+        } else if (numb == 92) {
+            printf("Число %d => девяносто два.\n", numb);
+        } else if (numb == 93) {
+            printf("Число %d => девяносто три.\n", numb);
+        } else if (numb == 94) {
+            printf("Число %d => девяносто четыре.\n", numb);
+        } else if (numb == 95) {
+            printf("Число %d => девяносто пять.\n", numb);
+        } else if (numb == 96) {
+            printf("Число %d => девяносто шесть.\n", numb);
+        } else if (numb == 97) {
+            printf("Число %d => девяносто семь.\n", numb);
+        } else if (numb == 98) {
+            printf("Число %d => девяносто восемь.\n", numb);
+        } else if (numb == 99) {
+            printf("Число %d => девяносто девять.\n", numb);
+        } else {
+            printf("Число %d => сто.\n", numb);
+        }
+    } else {
+        printf("Дружише прочитай заголовок!!!\n");
+    }
+
+    return 0;
+}
---
+++ lesson3/c_c++/homework/homework3_9.cpp
@@ -0,0 +1,216 @@
+#include <iostream>
+
+int main() {
+    using namespace std;
+    int numb;
+
+    cout << "Введите число от 1 до 100!" << endl;
+    cout << "Введите число => ";
+    cin >> numb;
+    
+    if (numb > 0 && numb < 101) {
+        if (numb == 1) {
+            cout << "Число => " << numb << " один."  << endl;
+        } else if (numb == 2) {
+            cout << "Число => " << numb << " два." << endl;
+        } else if (numb == 3) {
+            cout << "Число => " << numb << " три." << endl;
+        } else if (numb == 4) {
+            cout << "Число => " << numb << " четыре." << endl;
+        } else if (numb == 5) {
+            cout << "Число => " << numb << " пять." << endl;
+        } else if (numb == 6) {
+            cout << "Число => " << numb << " шесть." << endl;
+        } else if (numb == 7) {
+            cout << "Число => " << numb << " семь." << endl;
+        } else if (numb == 8) {
+            cout << "Число => " << numb << " восемь." << endl;
+        } else if (numb == 9) {
+            cout << "Число => " << numb << " девять." << endl;
+        } else if (numb == 10) {
+            cout << "Число => " << numb << " десять." << endl;
+        } else if (numb == 11) {
+            cout << "Число => " << numb << " одинадцать." << endl;
+        } else if (numb == 12) {
+            cout << "Число => " << numb << " двенадцать." << endl;
+        } else if (numb == 13) {
+            cout << "Число => " << numb << " тринадцать." << endl;
+        } else if (numb == 14) {
+            cout << "Число => " << numb << " четырнадцать." << endl;
+        } else if (numb == 15) {
+            cout << "Число => " << numb << " петнадцать." << endl;
+        } else if (numb == 16) {
+            cout << "Число => " << numb << " шестнацать." << endl;
+        } else if (numb == 17) {
+            cout << "Число => " << numb << " семнадцать." << endl;
+        } else if (numb == 18) {
+            cout << "Число => " << numb << " восемнадцать." << endl;
+        } else if (numb == 19) {
+            cout << "Число => " << numb << " деветнадцать." << endl;
+        } else if (numb == 20) {
+            cout << "Число => " << numb << " двадцать." << endl;
+        } else if (numb == 21) {
+            cout << "Число => " << numb << " двадцать один." << endl;
+        } else if (numb == 22) {
+            cout << "Число => " << numb << " двадцать два." << endl;
+        } else if (numb == 23) {
+            cout << "Число => " << numb << " двадцать три." << endl;
+        } else if (numb == 24) {
+            cout << "Число => " << numb << " двадцать четыре." << endl;
+        } else if (numb == 25) {
+            cout << "Число => " << numb << " двадцать пять." << endl;
+        } else if (numb == 26) {
+            cout << "Число => " << numb << " двадцать шесть." << endl;
+        } else if (numb == 27) {
+            cout << "Число => " << numb << " двадцать семь." << endl;
+        } else if (numb == 28) {
+            cout << "Число => " << numb << " двадцать восемь." << endl;
+        } else if (numb == 29) {
+            cout << "Число => " << numb << " двадцать девять." << endl;
+        } else if (numb == 30) {
+            cout << "Число => " << numb << " тридцать." << endl;
+        } else if (numb == 31) {
+            cout << "Число => " << numb << " тридцать один." << endl;
+        } else if (numb == 32) {
+            cout << "Число => " << numb << " тридцать два." << endl;
+        } else if (numb == 33) {
+            cout << "Число => " << numb << " тридцать три." << endl;
+        } else if (numb == 34) {
+            cout << "Число => " << numb << " тридцать четыре." << endl;
+        } else if (numb == 35) {
+            cout << "Число => " << numb << " тридцать пять." << endl;
+        } else if (numb == 36) {
+            cout << "Число => " << numb << " тридцать шесть." << endl;
+        } else if (numb == 37) {
+            cout << "Число => " << numb << " тридцать семь." << endl;
+        } else if (numb == 38) {
+            cout << "Число => " << numb << " тридцать восемь." << endl;
+        } else if (numb == 39) {
+            cout << "Число => " << numb << " тридцать девять." << endl;
+        } else if (numb == 40) {
+            cout << "Число => " << numb << " сорок." << endl;
+        } else if (numb == 41) {
+            cout << "Число => " << numb << " сорок один." << endl;
+        } else if (numb == 42) {
+            cout << "Число => " << numb << " сорок два." << endl;
+        } else if (numb == 43) {
+            cout << "Число => " << numb << " сорок три." << endl;
+        } else if (numb == 44) {
+            cout << "Число => " << numb << " сорок четыре." << endl;
+        } else if (numb == 45) {
+            cout << "Число => " << numb << " сорок пять." << endl;
+        } else if (numb == 46) {
+            cout << "Число => " << numb << " сорок шесть." << endl;
+        } else if (numb == 47) {
+            cout << "Число => " << numb << " сорок семь." << endl;
+        } else if (numb == 48) {
+            cout << "Число => " << numb << " сорок восемь." << endl;
+        } else if (numb == 49) {
+            cout << "Число => " << numb << " сорок девять." << endl;
+        } else if (numb == 50) {
+            cout << "Число => " << numb << " пятьдесят." << endl;
+        } else if (numb == 51) {
+            cout << "Число => " << numb << " пятьдесят один." << endl;
+        } else if (numb == 52) {
+            cout << "Число => " << numb << " пятьдесят два." << endl;
+        } else if (numb == 53) {
+            cout << "Число => " << numb << " пятьдесят три." << endl;
+        } else if (numb == 54) {
+            cout << "Число => " << numb << " пятьдесят четыре." << endl;
+        } else if (numb == 55) {
+            cout << "Число => " << numb << " пятьдесят пять." << endl;
+        } else if (numb == 56) {
+            cout << "Число => " << numb << " пятьдесят шесть." << endl;
+        } else if (numb == 57) {
+            cout << "Число => " << numb << " пятьдесят семь." << endl;
+        } else if (numb == 58) {
+            cout << "Число => " << numb << " пятьдесят восемь." << endl;
+        } else if (numb == 59) {
+            cout << "Число => " << numb << " пятьдесят деветь." << endl;
+        } else if (numb == 60) {
+            cout << "Число => " << numb << " шетьдесят." << endl;
+        } else if (numb == 61) {
+            cout << "Число => " << numb << " шетьдесят один." << endl;
+        } else if (numb == 62) {
+            cout << "Число => " << numb << " шетьдесят два." << endl;
+        } else if (numb == 63) {
+            cout << "Число => " << numb << " шетьдесят три." << endl;
+        } else if (numb == 64) {
+            cout << "Число => " << numb << " шетьдесят четыре." << endl;
+        } else if (numb == 65) {
+            cout << "Число => " << numb << " шетьдесят пять." << endl;
+        } else if (numb == 66) {
+            cout << "Число => " << numb << " шетьдесят шесть." << endl;
+        } else if (numb == 67) {
+            cout << "Число => " << numb << " шетьдесят семь." << endl;
+        } else if (numb == 68) {
+            cout << "Число => " << numb << " шетьдесят восемь." << endl;
+        } else if (numb == 69) {
+            cout << "Чсило => " << numb << " шетьдесят девять." << endl;
+        } else if (numb == 70) {
+            cout << "Число => " << numb << " семьдесят." << endl;
+        } else if (numb == 71) {
+            cout << "Число => " << numb << " семьдесят один." << endl;
+        } else if (numb == 72) {
+            cout << "Число => " << numb << " семьдесят два." << endl;
+        } else if (numb == 73) {
+            cout << "Число => " << numb << " семьдесят три." << endl;
+        } else if (numb == 74) {
+            cout << "Число => " << numb << " семьдесят четыре." << endl;
+        } else if (numb == 75) {
+            cout << "Число => " << numb << " семьдесят пять." << endl;
+        } else if (numb == 76) {
+            cout << "Число => " << numb << " семьдесят шесть." << endl;
+        } else if (numb == 78) {
+            cout << "Число => " << numb << " семьдесят семь." << endl;
+        } else if (numb == 79) {
+            cout << "Число => " << numb << " семьдесят восемь." << endl;
+        } else if (numb == 80) {
+            cout << "Число => " << numb << " восемьдесят." << endl;
+        } else if (numb == 81) {
+            cout << "Число => " << numb << " восемьдесят один." << endl;
+        } else if (numb == 82) {
+            cout << "Число => " << numb << " восемьдесят два." << endl;
+        } else if (numb == 83) {
+            cout << "Число => " << numb << " восемьдесят три." << endl;
+        } else if (numb == 84) {
+            cout << "Число => " << numb << " восемьдесят четыре." << endl;
+        } else if (numb == 85) {
+            cout << "Число => " << numb << " восемьдесят пять." << endl;
+        } else if (numb == 86) {
+            cout << "Число => " << numb << " восемьдесят шесть." << endl;
+        } else if (numb == 87) {
+            cout << "Число => " << numb << " восемьдесят семь." << endl;
+        } else if (numb == 88) {
+            cout << "Число => " << numb << " восемьдесят восемь." << endl;
+        } else if (numb == 89) {
+            cout << "Число => " << numb << " восемьдесят девять." << endl;
+        } else if (numb == 90) {
+            cout << "Число => " << numb << " девяносто." << endl;
+        } else if (numb == 91) {
+            cout << "Число => " << numb << " девяносто один." << endl;
+        } else if (numb == 92) {
+            cout << "Число => " << numb << " девяносто два." << endl;
+        } else if (numb == 93) {
+            cout << "Число => " << numb << " девяносто три." << endl;
+        } else if (numb == 94) {
+            cout << "Число => " << numb << " девяносто четыре." << endl;
+        } else if (numb == 95) {
+            cout << "Число => " << numb << " девяносто пять." << endl;
+        } else if (numb == 96) {
+            cout << "Число => " << numb << " девяносто шесть." << endl;
+        } else if (numb == 97) {
+            cout << "Число => " << numb << " девяносто семь." << endl;
+        } else if (numb == 98) {
+            cout << "Число => " << numb << " девяносто восемь." << endl;
+        } else if (numb == 99) {
+            cout << "Число => " << numb << " девяносто девять." << endl;
+        } else {
+            cout << "Число => " << numb << " сто." << endl;
+        }
+    } else {
+        cout << "Дружище читай лечше заголовок!!!!" << endl;
+    }
+    
+    return 0;
+}
