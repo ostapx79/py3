583b0caafbc5395acc7a4351d942126a8296f0d8
ConstantinGalus <ostapx79@gmail.com>
2020-10-30 15:10:07 +0300

    д/з по Си Си++ и Python3, урока 3 упражнение 9, готова

--- lesson3/python3/homework/homework3_9.py
+++ lesson3/python3/homework/homework3_9.py
@@ -51,9 +51,11 @@ if 0 < numb < 101:
         print(numb_string + str(numb) + " дватцать два.")
     elif numb == 23:
         print(numb_string + str(numb) + " дватцать три.")
-    elif numb == 25:
+    elif numb == 24:
         print(numb_string + str(numb) + " дватцать четыре.")
-    elif nubm == 26:
+    elif numb == 25:
+        print(numb_string + str(numb) + " дватцать пять.")
+    elif numb == 26:
         print(numb_string + str(numb) + " дватцать шесть.")
     elif numb == 27:
         print(numb_string + str(numb) + " дватцать семь.")
@@ -63,5 +65,145 @@ if 0 < numb < 101:
         print(numb_string + str(numb) + " дватцать девять.")
     elif numb == 30:
         print(numb_string + str(numb) + " тридцать.")
+    elif numb == 31:
+        print(numb_string + str(numb) + " тридцать один.")
+    elif numb == 32:
+        print(numb_string + str(numb) + " тридцать два.")
+    elif numb == 33:
+        print(numb_string + str(numb) + " тридцать три.")
+    elif numb == 34:
+        print(numb_string + str(numb) + " тридцать четыре.")
+    elif numb == 35:
+        print(numb_string + str(numb) + " тридцать пять.")
+    elif numb == 36:
+        print(numb_string + str(numb) + " тридцать шесть.")
+    elif numb == 37:
+        print(numb_string + str(numb) + " тридцать семь.")
+    elif numb == 38:
+        print(numb_string + str(numb) + " тридцать восемь.")
+    elif numb == 39:
+        print(numb_string + str(numb) + " тридцать девять.")
+    elif numb == 40:
+        print(numb_string + str(numb) + " сорок.")
+    elif numb == 41:
+        print(numb_string + str(numb) + " сорок один.")
+    elif numb == 42:
+        print(numb_string + str(numb) + " сорок два.")
+    elif numb == 43:
+        print(numb_string + str(numb) + " сорок три.")
+    elif numb == 44:
+        print(numb_string + str(numb) + " сорок четыре.")
+    elif numb == 45:
+        print(numb_string + str(numb) + " сорок пять.")
+    elif numb == 46:
+        print(numb_string + str(numb) + " сорок шесть.")
+    elif numb == 47:
+        print(numb_string + str(numb) + " сорок семь.")
+    elif numb == 48:
+        print(numb_string + str(numb) + " сорок восемь.")
+    elif numb == 49:
+        print(numb_string + str(numb) + " сорок девять.")
+    elif numb == 50:
+        print(numb_string + str(numb) + " пятьдесят.")
+    elif numb == 51:
+        print(numb_string + str(numb) + " пятьдесят один.")
+    elif numb == 52:
+        print(numb_string + str(numb) + " пятьдесят два.")
+    elif numb == 53:
+        print(numb_string + str(numb) + " пятьдесят три.")
+    elif numb == 54:
+        print(numb_string + str(numb) + " пятьдесят четыре.")
+    elif numb == 55:
+        print(numb_string + str(numb) + " пятьдесят пять.")
+    elif numb == 56:
+        pirnt(numb_string + str(numb) + " пятьдесят шесть.")
+    elif numb == 57:
+        print(numb_string + str(numb) + " пятьдесят семь.")
+    elif numb == 58:
+        print(numb_string + str(numb) + " пятьдесят восемь.")
+    elif numb == 59:
+        print(numb_string + str(numb) + " пятьдесят девять.")
+    elif numb == 60:
+        print(numb_string + str(numb) + " шестьдесят.")
+    elif numb == 61:
+        print(numb_string + str(numb) + " шестьдесят один.")
+    elif numb == 62:
+        print(numb_string + str(numb) + " шестьдесят два.")
+    elif numb == 63:
+        print(numb_string + str(numb) + " шестьдесят три.")
+    elif numb == 64:
+        print(numb_string + str(numb) + " шестьдесят четыре.")
+    elif numb == 65:
+        print(numb_string + str(numb) + " шестьдесят пять.")
+    elif numb == 66:
+        print(numb_string + str(numb) + " шестьдесят шесть.")
+    elif numb == 67:
+        print(numb_string + str(numb) + " шестьдесят семь.")
+    elif numb == 68:
+        print(numb_string + str(numb) + " шестьдесят восемь.")
+    elif numb == 69:
+        pirnt(numb_string + str(numb) + " шестьдесят девять.")
+    elif numb == 70:
+        print(numb_string + str(numb) + " семьдесят.")
+    elif numb == 71:
+        print(numb_string + str(numb) + " семьдесят один.")
+    elif numb == 72:
+        print(numb_string + str(numb) + " семьдесят два.")
+    elif numb == 73:
+        print(numb_string + str(numb) + " семьдесят три.")
+    elif numb == 74:
+        print(numb_string + str(numb) + " семьдесят четыре.")
+    elif numb == 75:
+        print(numb_string + str(numb) + " семьдесят пять.")
+    elif numb == 76:
+        print(numb_string + str(numb) + " семьдесят шесть.")
+    elif numb == 77:
+        print(numb_string + str(numb) + " семьдесят семь.")
+    elif numb == 78:
+        print(numb_string + str(numb) + " семьдесят восемь.")
+    elif numb == 79:
+        print(numb_string + str(numb) + " семьдесят девять.")
+    elif numb == 80:
+        print(numb_string + str(numb) + " восемьдесят.")
+    elif numb == 81:
+        print(numb_string + str(numb) + " восемьдесят один.")
+    elif numb == 82:
+        print(numb_string + str(numb) + " восемьдесят два.")
+    elif numb == 83:
+        print(numb_string + str(numb) + " восемьдесят три.")
+    elif numb == 84:
+        print(numb_string + str(numb) + " восемьдесят четыре.")
+    elif numb == 85:
+        print(numb_string + str(numb) + " восемьдесят пять.")
+    elif numb == 86:
+        print(numb_string + str(numb) + " восемьдесят шесть.")
+    elif numb == 87:
+        print(numb_string + str(numb) + " восемьдесят семь.")
+    elif numb == 88:
+        print(numb_string + str(numb) + " восемьдесят восемь.")
+    elif numb == 89:
+        print(numb_string + str(numb) + " восемьдесят девять.")
+    elif numb == 90:
+        print(numb_string + str(numb) + " девяносто.")
+    elif numb == 91:
+        print(numb_string + str(numb) + " девяносто один.")
+    elif numb == 92:
+        print(numb_string + str(numb) + " девяносто два.")
+    elif numb == 93:
+        print(numb_string + str(numb) + " девяносто три.")
+    elif numb == 94:
+        print(numb_string + str(numb) + " девяносто четыре.")
+    elif numb == 95:
+        print(numb_string + str(numb) + " девяносто пять.")
+    elif numb == 96:
+        print(numb_string + str(numb) + " девяносто шесть.")
+    elif numb == 97:
+        print(numb_string + str(numb) + " девяносто семь.")
+    elif numb == 98:
+        print(numb_string + str(numb) + " девяносто восемь.")
+    elif numb == 99:
+        print(numb_string + str(numb) + " девяносто девять.")
+    else:
+        print(numb_string + str(numb) + " сто.")
 else:
     print("Дружище прочитай заголовок!!!")
