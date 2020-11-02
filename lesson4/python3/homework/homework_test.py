7519c832a2c42d679f97cc82e9d57f4730752976
ConstantinGalus <ostapx79@gmail.com>
2020-11-02 21:54:05 +0300

    цыклы в Си Си++ и Python3

--- lesson4/python3/homework/homework_test.py
+++ lesson4/python3/homework/homework_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # На вход даются данные, парни и девченки покупают билеты в кинотятр, и мы
-# должны раставаить по местам где, парень девченка парень девченка или наоборот.
+# должны раставаить их по местам, где, парень девченка парень девченка или наоборот.
 # пример:
 # mens = 3
 # women = 3
@@ -10,9 +10,41 @@
 guys = 3
 girls = 3
 
-numb = 1
-while numb < guys:
-    for gu in range(1, guys+1):
-        print("numb =", gu)
-    numb += 1
+if guys == girls:
+    i = guys + girls
+    while i != 0:
+        if guys >= girls:
+            gu = 'gu'
+            print(gu, end='')
+            guys -= 1
+        else:
+            gi = 'gi'
+            print(gi, end='')
+            girls -= 1
+        i -= 1
+print()
+guys = 5
+girls = 4
 
+if guys > girls:
+    i = guys + girls
+    while i != 0:
+        if guys > girls:
+            gu = 'gu'
+            print(gu, end='')
+            guys -= 1
+        elif girls > guys:
+            gi = 'gi'
+            print(gi, end='')
+            girls -= 1
+        else:
+            if girls == guys and guys == girls:
+                gi = 'gi'
+                print(gi, end='')
+                girls -= 1
+            else:
+                gu = 'gu'
+                print(gu, end='')
+                guys -= 1
+        i -= 1
+print()
---
+++ lesson4/python3/lesson4_12.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+for numb_i in range(1, 5):
+    for numb_k in range(1, numb_i+1):
+        print("numb_k =", numb_k)
+    print("numb_i =", numb_i)
