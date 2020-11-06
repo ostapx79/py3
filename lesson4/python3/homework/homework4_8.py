9407604e946a68fa9e756529a37f624212737977
ConstantinGalus <ostapx79@gmail.com>
2020-11-06 14:59:17 +0300

    модификация д/з

--- lesson4/python3/homework/homework4_8.py
+++ lesson4/python3/homework/homework4_8.py
@@ -6,7 +6,7 @@ numb_rand = int(input("Введите число => "))
 # result = 0.0
 
 numb_i = 0
-while numb_i <= numb_rand:
-    result = random.uniform(0.0, 1.0)
-    print("Псевдослучаное число от 0 до 1 =>", result)
+while numb_i < numb_rand:
+    result = random.uniform(0, 1)
+    print("Псевдослучаное число от 0 до 1 =>", "{: .2f}".format(result))
     numb_i += 1
