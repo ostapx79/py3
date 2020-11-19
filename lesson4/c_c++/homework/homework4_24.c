54ab39f2a1c52bfb723fde8c8cff15f7986659d1
Constantin Galus <ostapx79@gmail.com>
2020-11-20 02:30:42 +0300

    программан не готова

--- lesson4/c_c++/homework/homework4_24.c
+++ lesson4/c_c++/homework/homework4_24.c
@@ -3,62 +3,28 @@
 // выведено число ноль. В конце программы на экран выводится минимальное и
 // максимальное из введенных чисел (не считая ноль).
 # include <stdio.h>
-# include <stdbool.h>
 
 int main() {
-    int numb_input_1;
-    printf("Введите число => ");
-    scanf("%d", &numb_input_1);
-    bool flag = true;
-    int numb_input_2;
     int numb_min, numb_max;
-    while (numb_input_2 != 0) {
-
-        scanf("%d", &numb_input_2);
-        if (numb_input_2 == 0) {
-            break;
+    numb_max = -10000;
+    numb_min = 10000;
+    int value_x;
+//    printf("Введите число input_1 => ");
+//    scanf("%d", &numb_max);
+//    printf("Введите число input_2 => ");
+//    scanf("%d", &numb_min);
+    printf("Введите число value_x1 => ");
+    scanf("%d", &value_x);
+    while (value_x != 0) {
+        if (value_x > numb_max) {
+//            value_x = numb_max;
+            numb_max = value_x;
+        } else if (value_x < numb_min) {
+            numb_min = value_x;
+            //value_x = numb_min;
         }
-        if (numb_input_1 < numb_input_2 && numb_min > numb_input_2) {
-            numb_min = numb_input_1;
-            numb_min = numb_input_2;
-            printf("numb_min => %d\n", numb_min);
-            if (numb_input_2 < numb_min) {
-                numb_min = numb_input_2;
-                printf("min => %d\n", numb_min);
-            }
-//            if (numb_min < numb_input_2) {
-//                numb_min = numb_input_1;
-////                printf("numb_input_1 => %d\n", numb_input_1);
-////                printf("numb_input_2 => %d\n", numb_input_2);
-////                printf("numb_min => %d\n", numb_min);
-//            } else {
-//                numb_min = numb_input_2;
-////                printf("numb_input_1 => %d\n", numb_input_1);
-////                printf("numb_input_2 => %d\n", numb_input_2);
-////                printf("numb_min => %d\n", numb_min);
-//            }
-//
-//            if (numb_input_1 > numb_input_2) {
-//                numb_max = numb_input_1;
-//                if (numb_input_2 > numb_max) {
-//                    numb_max = numb_input_2;
-//                }
-////                printf("numb_input_1 => %d\n", numb_input_1);
-////                printf("numb_input_2 => %d\n", numb_input_2);
-////                printf("numb_min => %d\n", numb_max);
-//            } else {
-//                numb_max = numb_input_2;
-////                printf("numb_input_1 => %d\n", numb_input_1);
-////                printf("numb_input_2 => %d\n", numb_input_2);
-////                printf("numb_min => %d\n", numb_max);
-//            }
-//
-        } else {
-            numb_min = numb_input_2;
-            printf("numb_foo => %d\n", numb_min);
-        }
-        printf("numb_min => %d\n", numb_min);
-        printf("numb_max => %d\n", numb_max);
+        printf("Введите число value_x2 => ");
+        scanf("%d", &value_x);
     }
     printf("numb_min => %d\n", numb_min);
     printf("numb_max => %d\n", numb_max);
