b2595ce0d74a18f167fab81329b51778132400b5
Constantin Galus <ostapx79@gmail.com>
2020-12-01 23:22:24 +0300

    параметры с изменяемыми значенями

--- lesson5/python3/lesson5_4.py
+++ lesson5/python3/lesson5_4.py
@@ -2,3 +2,15 @@
 # Процедуры с изменяемыми параметрами.
 
 
+def swaps(numb_a, numb_b):
+    numb_c = numb_a
+    numb_a = numb_b
+    numb_b = numb_c
+    print(numb_a, numb_b)
+
+
+numb_x = int(input("Введите натуральное число -> "))
+numb_y = int(input("Введите натуральное число -> "))
+
+swaps(numb_x, numb_y)
+
