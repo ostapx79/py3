b0c32ce408ef85b6dc2d29b56d0664f824777671
Constantin Galus <ostapx79@gmail.com>
2021-01-08 02:21:13 +0300

    дорабатоваем д/з

--- lesson6/python3/homework/homework6_10a.py
+++ lesson6/python3/homework/homework6_10a.py
@@ -1,8 +1,18 @@
-res1 = 25
-res2 = 0
-for i in range(1, res1):
-    #print('i -', i)
-    for j in range(1, i+1):
-        #print('j -', j)
-        if i % j == 0:
-            print(j)
+#res1 = 25
+#res2 = 0
+#for i in range(1, res1):
+#    print('i -', i)
+#    for j in range(1, i):
+#        #print('j -', j)
+#        if i % j == 0:
+#            res2 += j
+#            print('res2 -', res2)
+res = 24
+res_s = 0
+for i_i in range(1, res):
+    if res % i_i == 0:
+        print('i_i == 0', i_i)
+        res_s += i_i
+    else:
+        print('i_i != 0', i_i)
+print('res_s -', res_s)
