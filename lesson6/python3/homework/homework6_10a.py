558e44df8a29205333e1b61c786f1a2e385bb828
ConstantinGalus <ostapx79@gmail.com>
2021-01-13 20:18:22 +0300

    д/з по Python3, урока 6 упражнение 10a

--- lesson6/python3/homework/homework6_10a.py
+++ lesson6/python3/homework/homework6_10a.py
@@ -1,32 +1,37 @@
-#!/usr/bin/python3
+#/usr/bin/python3
 
 
-def friendly_number(result1, result2):
-    i = 0
+def friendly_number1(result1):
+    i = 1
     res_s = 0
-    while i <= result1:
+    while i < result1:
         if result1 % i == 0:
             res_s += i
         i += 1
+    return res_s
 
-    j = 0
+
+def friendly_number2(result2):
+    j = 1
     res_r = 0
-    while j <= result2:
+    while j < result2:
         if result2 % j == 0:
             res_r += j
         j += 1
-
-    if res_s == result2 and res_r == result1:
-        return 'дружественный число!'
-    else:
-        return 'не дружественные число'
+    return res_r
 
 
-# result1 = 220
-result = 285
+def friendly_numb3(result):
+    for i in range(1, result+1):
+        res_i = friendly_number1(i)
+        for j in range(i):
+            res_j = friendly_number2(j)
 
-for i in range(result):
-    for j in range(i):
-        res = friendly_number(i, j)
-        print(res)
+            if res_i == j and res_j == i:
+                print('дружественный число!')
+                print(res_i, res_j)
+
+
+result = 1285
+friendly_numb3(result)
 
