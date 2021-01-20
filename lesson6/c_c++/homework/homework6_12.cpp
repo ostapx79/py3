581d76a50391cdcbb5595aa698523b4b854e57d5
ConstantinGalus <ostapx79@gmail.com>
2021-01-20 14:35:02 +0300

    д/з по Си++, урока 6 упражнение 12

--- lesson6/c_c++/homework/homework6_12.cpp
+++ lesson6/c_c++/homework/homework6_12.cpp
@@ -1,7 +1,7 @@
 // lesson6/c_c++/homework/homework6_12.cpp
-#include <stdio.h>
+#include <iostream>
 
-int is_prime(int numb) {
+bool is_prime(int numb) {
     int res_d = 2;
     while (res_d * res_d <= numb && numb % res_d != 0) {
         res_d++;
@@ -9,13 +9,27 @@ int is_prime(int numb) {
     return (res_d * res_d > numb);
 }
 
+int hyper_simple(int numb_i) {
+    int i = 1;
+    int res, res_1;
+    res = res_1 = 0;
+    while (numb_i > i) {
+        res = is_prime(numb_i);
+        if (res == 1) {
+            res_1 += res;
+        }
+        numb_i /= 10;
+    }
+    return res_1;
+}
+
 int main() {
-    int numb = 500;
+    int numb = 1000;
     int i;
     for (i = 2; i < numb; i++) {
-        int result = is_prime(i);
-        if (result == 1) {
-            printf("is_prime -> %d\n", i);
+        int result = hyper_simple(i);
+        if (result == 3) {
+            std::cout << "hyper_simple -> " << i << std::endl;
         }
     }
     return 0;
