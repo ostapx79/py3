89cbe6bdc1064564361b8af4f101ebdeff4f7fe4
ConstantinGalus <ostapx79@gmail.com>
2021-02-01 12:13:13 +0300

    Ханойские башни на Python3

--- lesson7/python3/lesson7_1.py
+++ lesson7/python3/lesson7_1.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-# Рекурсия в Python-3
+# Ханойские башни на Python-3
 
-def recursion():
-    pass
 
+def hanoi(numb_n, numb_k, numb_m):
+    if numb_n == 0: return
+
+    numb_p = 6 - numb_k - numb_m
+    hanoi(numb_n - 1, numb_k, numb_p)
+    print(str(numb_k) + " -> " + str(numb_m))
+    hanoi(numb_n - 1, numb_p, numb_m)
+
+
+hanoi(4, 1, 3)
 
